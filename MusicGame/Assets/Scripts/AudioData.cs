b2823ee3327ffc21c36eebed2586fecdd3b228f3
CHI-KUN\chi_k <chi_kun58@icloud.com>
2026-06-22 02:19:34 +0900

    clipを生成する際に必要なデータを持つAudioDataクラスを作成

---
+++ MusicGame/Assets/Scripts/AudioData.cs
@@ -0,0 +1,8 @@
+using UnityEngine;
+
+[System.Serializable]
+public class AudioData
+{
+	public int soundHertz;
+	public string soundName;
+}
---
+++ MusicGame/Assets/Scripts/SoundGenerater.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundGenerater : MonoBehaviour
+{
+    [SerializeField] private List<AudioData> audioDatas = new();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
